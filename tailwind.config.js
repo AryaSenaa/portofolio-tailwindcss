857fe3660ab36c2dac22ee8f700ed5ec7947733c
AryaSenaa <senju5546@gmail.com>
2022-06-12 09:54:30 +0700

    portofolio tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#d946ef',
+        secondary: '#a1a1aa',
+        dark: '#0f172a',
+      },
+      screens:{
+        'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
+      },
+    },
+  },
+  plugins: [],
+}
