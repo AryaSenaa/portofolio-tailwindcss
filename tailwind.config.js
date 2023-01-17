f7caf83ce57475007954751ee9e23477a04f8f61
AryaSenaa <senju5546@gmail.com>
2023-01-17 10:56:31 +0700

    Portofolio menggunakan Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  content: ['index.html'],
+  darkMode: 'class',
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#c7bca1',
+        secondary: '#a1a1aa',
+        dark: '#18181b',
+      },
+      screens:{
+        'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
+      },
+    },
+  },
+  plugins: [],
+}
